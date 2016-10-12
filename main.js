f2d7f83427c1fb9ef58a3bc463ccf3a712029fda
John Socratous <John Socratous>
2016-10-12 13:57:18 +0300

    Added var roleTower

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
+var roleTower = require('role.tower');
 
 module.exports.loop = function () {
 
@@ -13,18 +14,26 @@ module.exports.loop = function () {
 
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     console.log('Harvesters: ' + harvesters.length);
-    
+
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
     console.log('Upgraders: ' + upgraders.length);
+
+    var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+    console.log('Builders: '+ builders.length);
 
     if(harvesters.length < 2 && Game.spawns['Spawn1'].energy >= 200) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
         console.log('Spawning new harvester: ' + newName);
     }
-    
+
     if(upgraders.length < 2 && Game.spawns['Spawn1'].energy >= 200) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
         console.log('Spawning new harvester: ' + newName);
+    }
+
+    if(builders.length < 2 && Game.spawns['Spawn1'].energy >= 200) {
+        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'builder'});
+        console.log('Spawning new builder: ' + newName);
     }
 
     for(var name in Game.creeps) {
