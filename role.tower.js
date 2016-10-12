2bd7c4da81adbbb594d00b89968e9ab804a7b316
BuildTools <unconfigured@null.spigotmc.org>
2016-10-12 09:56:17 +0300

    1st try

---
+++ role.tower.js
@@ -0,0 +1,25 @@
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports.thing = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('role.atkTower');
+ * mod.thing == 'a thing'; // true
+ */
+var roleTower = {
+    run: function(tower){
+        if(tower) {
+            var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+            var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+            if(closestHostile) {
+                tower.attack(closestHostile);
+            }
+        }
+    }
+}
+module.exports = roleTower;
